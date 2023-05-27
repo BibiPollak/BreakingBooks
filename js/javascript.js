40195c18ffd2d42e75e6c982211f12d2d27b898f
Gabriele Pollak <gabriele.santos@5by5.com.br>
2023-05-27 00:10:47 -0300

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,34 @@
+function showHome() {
+    window.location.href = "index.html";
+}
+function showVendidos() {
+    window.location.href = "MaisVendidos.html";
+}
+function showPromotions() {
+    window.location.href = "Promotion.html";
+}
+function showCarrinho() {
+    window.location.href = "carrinho.html";
+}
+function showLogin() {
+    window.location.href = "login.html";
+}
+function showCadastro() {
+    window.location.href = "cadastro.html";
+}
+
+
+//Botoes dentro de Generos//
+
+function showFantasy() {
+    window.location.href = "Fantasia.html";
+}
+function showHorror() {
+    window.location.href = "Terror.html";
+}
+function showAutoras() {
+    window.location.href = "Autoras.html";
+}
+function showLGBT() {
+    window.location.href = "LGBT.html";
+}
